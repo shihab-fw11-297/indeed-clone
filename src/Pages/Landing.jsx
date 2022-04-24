f3b117e0cca5e57e915ba376b8eb2f81fdd39872
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-04-24 15:39:23 +0530

    completed landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,15 +2,34 @@ import Header from '../Components/Header';
 import {Footer} from '../Components/Footer';
 import { Optioncard } from "../Components/Optioncard";
 import styles from "./landing.module.css";
+import {ResumeUpload} from '../Components/ResumeUpload';
 
 import { BsSearch } from "react-icons/bs";
 import { MdLocationOn } from "react-icons/md";
-import { useRef } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 import { Link } from 'react-router-dom';
+import {jobs }from '../jobs' 
 
 const Landing = () => {
+    const [search, setSearch] = useState([]);
+    const [query, setQuery] = useState("");
+    const options = useRef();
     const buttref = useRef();
 
+    const showdata = (e) =>{
+        setQuery(e.target.value.toLowerCase())
+         setSearch(jobs)
+    }
+
+    const manage = (e) => {
+        let searchQuery = e.target.innerText
+        localStorage.setItem("searchQuery", searchQuery)
+
+       options.current.value = e.target.innerText
+       buttref.current.style.height = "50px"
+       buttref.current.style.overflowY = "none"
+    }
+    
     return (
         <div className="sd">
             <div className={styles.head}>
@@ -27,33 +46,48 @@ const Landing = () => {
                 <div style={{ display: "flex" }}>
                     <div className={styles.area}>
                         <form className={styles.forms} action="">
-                            <div ref={buttref} className={styles.deb}>
+                        
+                        <div ref={buttref} className={styles.deb}>
                                 <div className={styles.search}>
                                     <div className={styles.lab}>What</div>
                                     <div className={styles.inp}>
                                         <input
                                             type="text"
-                                            //ref={options}
+                                            ref={options}
                                             placeholder="Job title, keywords, or company"
                                             className={styles.inpu}
-                                        //  onChange={optimisedVersion}
+                                            onChange={showdata}
+
                                         />
                                     </div>
                                     <div style={{ paddingRight: "0rem", paddingTop: '3px' }}>
                                         <BsSearch style={{ width: 15 }} />{" "}
                                     </div>
                                 </div>
-
+                                <div>
+                                {
+                                query?.length > 1 ? (
+                                    <div className={styles.autocomplete}>
+                                        { search.filter((asd) =>
+                                            asd.job.toLowerCase().includes(query)
+                                         ).map((el, i) => (
+                                            <div key={i} className={styles.autocompleteItems}>
+                                                <span onClick={manage}>{el.job}</span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ) : ""}
+                                </div>
                             </div>
 
                             <div className={styles.deb2}>
-                                <div className={styles.search}>
-                                    <div className={styles.lab}>Where</div>
-                                    <div className={styles.inp}>
+                                <div className={styles.search2}>
+                                    <div className={styles.lab2}>Where</div>
+                                    <div className={styles.inp2}>
                                         <input
                                             type="text"
                                             placeholder="City,state,or pin code"
-                                            className={styles.inpu}
+                                            className={styles.inpu2}
                                         />
                                     </div>
                                     <div style={{ paddingRight: "0rem", paddingTop: '3px' }}>
@@ -107,7 +141,7 @@ const Landing = () => {
             <div className={styles.resume}>
                 <ResumeUpload></ResumeUpload>
             </div>
-            
+
         </div>
     )
 }
