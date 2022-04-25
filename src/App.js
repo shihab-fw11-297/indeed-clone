f88b4d660c32d056dc2937fb48021751513ac9d1
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-04-25 17:05:47 +0530

    completed login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from './Pages/Landing';
 import {SignUp} from './Pages/SignUp';
+import {Login} from './Pages/Login';
 
 function App() {
   return (
@@ -16,6 +17,15 @@ function App() {
           <Route path="/signup">
             <SignUp />
           </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route exact path="/login">
+              <Login />
+          </Route>
+          
         </Switch>
       </Router>
     </div>
